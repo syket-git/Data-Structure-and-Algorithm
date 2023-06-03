0923ad229cb88aaf3103cda6295220d6e03b4ba1
Syket Bhattachergee <39830305+syket-git@users.noreply.github.com>
2023-06-03 08:58:51 +0600

    Create BookAllocationProblem.cpp

---
+++ BookAllocationProblem.cpp
@@ -0,0 +1,43 @@
+bool isPossible(vector<int>arr, int n, int m, int mid) {
+  int studentCount = 1;
+  int pageSum = 0;
+
+  for (int i = 0; i < n; i++) {
+
+    if (pageSum + arr[i] <= mid) {
+      pageSum += arr[i];
+    } else {
+      studentCount++;
+      if (studentCount > m || arr[i] > mid) {
+        return false;
+      }
+      pageSum = arr[i];
+    }
+  }
+  return true;
+}
+
+int findPages(vector<int> &arr, int n, int m) {
+
+  int start = 0;
+  int sum = 0;
+  for (int i = 0; i < n; i++) {
+    sum += arr[i];
+  }
+  int end = sum;
+  int mid = start + (end - start) / 2;
+  int ans = -1;
+  if(m > n){
+      return -1;
+  }
+  while (start <= end) {
+    if (isPossible(arr, n, m, mid)) {
+      ans = mid;
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+    mid = start + (end - start) / 2;
+  }
+  return ans;
+}
