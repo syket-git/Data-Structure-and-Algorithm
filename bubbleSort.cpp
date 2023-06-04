8c165f0ae55aec9c108ba1887e3c14c879c8a73d
Syket Bhattachergee <39830305+syket-git@users.noreply.github.com>
2023-06-04 09:18:44 +0600

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,11 +1,15 @@
-#include <bits/stdc++.h> 
-void bubbleSort(vector<int>& arr, int n)
-{   
-    for(int i = 1; i < n; i++){
-        for(int j = 0; j < n - i; j++){
-            if(arr[j] > arr[j + 1]){
-                swap(arr[j], arr[j+1]);
-            }
-        }
+#include <bits/stdc++.h>
+void bubbleSort(vector<int> &arr, int n) {
+  bool isSwapped = false;
+  for (int i = 1; i < n; i++) {
+    for (int j = 0; j < n - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        isSwapped = true;
+        swap(arr[j], arr[j + 1]);
+      }
     }
+    if (isSwapped == false) {
+      break;
+    }
+  }
 }
