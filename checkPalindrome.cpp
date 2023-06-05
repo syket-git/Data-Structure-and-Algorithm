c5ec87ff58781bcb6c6cbab2121e418b040f7d8a
Syket Bhattachergee <39830305+syket-git@users.noreply.github.com>
2023-06-05 23:26:29 +0600

    Create checkPalindrome.cpp

---
+++ checkPalindrome.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int getLength(char a[]) {
+  int count = 0;
+  for (int i = 0; a[i] != '\0'; i++) {
+    count++;
+  }
+  return count;
+}
+
+void reverseString(char name[], int len) {
+  int start = 0;
+  int end = len - 1;
+
+  while (start <= end) {
+    swap(name[start++], name[end--]);
+  }
+}
+
+bool checkPalindromes(char name[], int len) {
+
+  int start = 0;
+  int end = len - 1;
+
+  while (start <= end) {
+
+    if (name[start] != name[end]) {
+      return false;
+    } else {
+      start++;
+      end--;
+    }
+  }
+  return true;
+}
+
+int main() {
+
+  char name[20];
+
+  cout << "Enter your name: ";
+  cin >> name;
+
+  int len = getLength(name);
+  reverseString(name, len);
+
+  cout << "Your reverse name is: " << name << endl;
+  cout << "Palindrome: " << checkPalindromes(name, len) << endl;
+}
